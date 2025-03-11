e6c3f0a150b49431dd7d5d45af65e983dbe2f50d
surajit77707 <surajitsingh77707@gmail.com>
2025-03-11 04:12:50 -0700

    Update total_user_chat_db.py

--- ChatBot/database/total_user_chat_db.py
+++ ChatBot/database/total_user_chat_db.py
@@ -1,4 +1,4 @@
-from ChatBot.database.chats import total_chats, total_users
+from ChatBot.database.chats import chats, users
 
 # Save or update a user in the database
 async def save_user(user_id: int, first_name: str, username: str = None):
@@ -7,7 +7,7 @@ async def save_user(user_id: int, first_name: str, username: str = None):
         "first_name": first_name,
         "username": username,
     }
-    await total_users.update_one(
+    await users.update_one(
         {"user_id": user_id},
         {"$set": user_data},
         upsert=True
@@ -15,16 +15,16 @@ async def save_user(user_id: int, first_name: str, username: str = None):
 
 # Get all users from the database
 async def get_all_users():
-    return await total_users.find().to_list(None)
+    return await users.find().to_list(None)
 
 # Check if a user exists in the database
 async def is_user_in_db(user_id: int):
-    user = await total_users.find_one({"user_id": user_id})
+    user = await users.find_one({"user_id": user_id})
     return user is not None
 
 # Get the total count of users
-async def get_total_users_count():
-    return await total_users.count_documents({})
+async def get_users_count():
+    return await users.count_documents({})
 
 # Save or update a chat in the database
 async def save_chat(chat_id: int, chat_title: str):
@@ -32,7 +32,7 @@ async def save_chat(chat_id: int, chat_title: str):
         "chat_id": chat_id,
         "chat_title": chat_title,
     }
-    await total_chats.update_one(
+    await chats.update_one(
         {"chat_id": chat_id},
         {"$set": chat_data},
         upsert=True
@@ -40,13 +40,13 @@ async def save_chat(chat_id: int, chat_title: str):
 
 # Get all chats from the database
 async def get_all_chats():
-    return await total_chats.find().to_list(None)
+    return await chats.find().to_list(None)
 
 # Check if a chat exists in the database
 async def is_chat_in_db(chat_id: int):
-    chat = await total_chats.find_one({"chat_id": chat_id})
+    chat = await chats.find_one({"chat_id": chat_id})
     return chat is not None
 
 # Get the total count of chats
-async def get_total_chats_count():
-    return await total_chats.count_documents({})
+async def get_chats_count():
+    return await chats.count_documents({})
